fcb9783c5cfa3ca0579e323b1c16af58f99cf07d
Killian <gillmann.killian@gmail.com>
2022-03-25 13:20:02 +0100

    exo not

---
+++ Percolation/Percolation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Percolation
+{
+    public class Percolation
+    {
+        private readonly bool[,] _open;
+        private readonly bool[,] _full;
+        private readonly int _size;
+        private bool _percolate;
+
+        public Percolation(int size)
+        {
+            if (size <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(size), size, "Taille de la grille négative ou nulle.");
+            }
+
+            _open = new bool[size, size];
+            _full = new bool[size, size];
+            _size = size;
+        }
+
+        private bool IsOpen(int i, int j)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsFull(int i, int j)
+        {
+            throw new NotImplementedException();
+        }
+
+        private List<KeyValuePair<int, int>> CloseNeighbors(int i, int j)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Open(int i, int j)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Percolation/PercolationSimulation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Percolation
+{
+    public struct PclData
+    {
+        /// <summary>
+        /// Moyenne 
+        /// </summary>
+        public double Mean { get; set; }
+        /// <summary>
+        /// Ecart-type
+        /// </summary>
+        public double StandardDeviation { get; set; }
+
+        public double RelativeStd { get; set; }
+    }
+
+    public class PercolationSimulation
+    {
+        public PclData MeanPercolationValue(int size, int t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double PercolationValue(int size)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
